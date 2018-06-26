b37f086dc4a26fb1f6da747a97bdc746c9d61753
Ilya Nemtsov <nemtsovilya@yahoo.com>
2018-06-25 23:13:18 -0700

    added modification to the logic

--- colorGame.js
+++ colorGame.js
@@ -5,8 +5,19 @@ let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let message = document.querySelector("#message");
 let h1 = document.querySelector("h1");
+let resetButton = document.querySelector("#reset");
 
 colorDisplay.textContent = pickedColor;
+
+resetButton.addEventListener("click", ()=>{
+	colors = generateRandomColors(6);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(let i=0; i<squares.length; i++){
+		squares[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = "#232323";
+})
 
 for(let i=0; i<squares.length; i++){
 
@@ -16,6 +27,7 @@ for(let i=0; i<squares.length; i++){
 		let clickedColor = squares[i].style.backgroundColor;
 		if(clickedColor === pickedColor){
 			message.textContent = "Correct";
+			resetButton.textContent = "play again?";
 			changeColors(clickedColor);
 			h1.style.backgroundColor = clickedColor;
 		} else{

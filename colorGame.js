bf76710f0964da55971915529f61ef892f1daa67
Ilya Nemtsov <nemtsovilya@yahoo.com>
2018-06-25 22:46:08 -0700

    added the logic

--- colorGame.js
+++ colorGame.js
@@ -1,16 +1,11 @@
-let colors = [
-		"rgb(255, 0, 0)",
-		"rgb(255, 255, 0)",
-		"rgb(0, 0, 255)",
-		"rgb(0, 255, 255)",
-		"rgb(0, 255, 0)",
-		"rgb(255, 0, 255)"
-		]
+let colors = generateRandomColors(6);
 
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let message = document.querySelector("#message");
+let h1 = document.querySelector("h1");
+
 colorDisplay.textContent = pickedColor;
 
 for(let i=0; i<squares.length; i++){
@@ -22,6 +17,7 @@ for(let i=0; i<squares.length; i++){
 		if(clickedColor === pickedColor){
 			message.textContent = "Correct";
 			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
 		} else{
 			squares[i].style.backgroundColor = "#232323";
 			message.textContent = "Try again"
@@ -36,4 +32,25 @@ function changeColors(color){
 }
 function pickColor(){
 	return colors[Math.floor(Math.random()*colors.length)];
+}
+
+function generateRandomColors(number) {
+	let arr = [];
+
+	for(let i=0; i< number; i++){
+		arr.push(randomColor());
+	}
+
+
+	return arr;
+}
+
+
+
+function randomColor() {
+	let r = Math.floor(Math.random()*256);
+	let g = Math.floor(Math.random()*256);
+	let b = Math.floor(Math.random()*256);
+	return "rgb("+ r +", "+ g +", "+ b +")"
+
 }
